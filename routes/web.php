5ada80f8a15483ed5af01552c513be68baf9cffc
Sajal Shrestha <codeitsajal@gmail.com>
2021-04-06 21:33:15 +0545

    minor changes

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $medecines = Product::where('category_id',1)->limit(4)->get();
-    $baby = Product::where('category_id',2)->limit(2)->get();
+    $baby = Product::where('category_id',2)->limit(4)->get();
     return view('frontend.pages.index',compact('medecines','baby'));
 });
 
